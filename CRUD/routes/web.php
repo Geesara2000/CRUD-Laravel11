081fe1c4594114e4f28df244574368f8a7c52b57
Geesara2000 <geesaraimal12@gmail.com>
2025-02-13 20:44:54 +0530

    update route

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/admin',[userController::class,'index'])->name('user.index');
+Route::get('/',[userController::class,'index'])->name('user.index');
 
 Route::group(['prefix'=>'admin/user'],function(){
     Route::get('/create',[userController::class,'create'])->name('user.create');
